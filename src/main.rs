96c853f5d791856d95b9dd917c5842bc22712055
niklas-gif <niklas.demel1@gmail.com>
2023-02-24 20:56:36 +0100

    Implemented word count.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::io;
 
+#[derive(Eq)]
+#[derive(Debug)]
 struct Word{
    pub word: String,
    pub count: i32
+}
+impl PartialEq for Word{
+    fn eq(&self, other: &Self) -> bool {
+        self.word == other.word
+    }
 }
 
 impl Word {
@@ -11,19 +18,17 @@ impl Word {
 
 fn main() {
     println!("Enter the text to display word count or type quite: to quite");
-    //let text : String = String::from("Hello");
-    let mut word = Word{
-        word: String::from("Chicken"),
-        count:0
-    };
     let mut input = String::new();
-    
+    let mut words:Vec<Word> = Vec::new();
     io::stdin().read_line(&mut input).unwrap();
     
+    if input.ends_with('\n') {
+         input = input[0..input.len() - 1].to_string();
+        }
+    
     if !input.contains("quite:") {
-        parse(&input,&mut word);
-        println!("Word:{} amount:{}",word.word,word.count);
-        println!("Text:{}",input);
+        parse(&input,&mut words);
+        print_words(&words);
     }
     else{
         println!("Bye Bye!");
@@ -31,11 +36,22 @@ fn main() {
     }
 }
 
-fn parse(text: &String,word : &mut Word){
+fn parse(text: &String,words:&mut Vec<Word>){
     let text_low = text.to_lowercase().clone();
-    for i in text_low.split(" "){
-        if i.contains(&word.word.to_lowercase()){
-            word.count = word.count + 1;
+    for text_word in text_low.split(" "){
+        if !(words.iter().any(|word| word.word.to_lowercase().eq(text_word))){
+            words.push(Word::new(String::from(text_word),0));
         }
-    } 
+        for i in 0..words.len(){
+        if words[i].word.to_lowercase().eq(text_word){
+            words[i].count = words[i].count + 1;
+        }
+    }
+   }
+}
+
+fn print_words(words:&Vec <Word>){
+    for word in words{
+        println!("Word: {} amount: {} \n------------------",word.word,word.count);
+    }
 }
